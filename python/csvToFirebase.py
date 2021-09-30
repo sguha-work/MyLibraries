4539f11826f1083063432e6d90319e01fee419ea
Sahasrangshu Guha <sahasrangshu.guha@ericsson.com>
2021-09-30 16:25:54 +0530

    work going on

--- python/csvToFirebase.py
+++ python/csvToFirebase.py
@@ -5,9 +5,21 @@ import json
 csvParserobj = CsvParser("sample-csv.csv")
 data = json.loads(csvParserobj.getJSONData())
 firebaseObj = FirebaseDB()
+# read entire collection example
+# firebaseObj.read('users')
 
 ## Bulk insert example
-# dataToWrite = {
-#     'items': data
-# }
-# firebaseObj.write(u'users',u'agents', dataToWrite)
+# firebaseObj.writeBulk(u'users',u'agents', data)
+## Single insert example
+firebaseObj.write(u'users',u'agents', {
+      "Sno": "1",
+      "Consumer ID": "2720629",
+      "Current Status": "Running ",
+      "Subdivision": "Jamtara ",
+      "Consumer No": "PDRCS00091 ",
+      "K No": "",
+      "Name": "M/s TOWER VISION INDIA ",
+      "Address": "C/o KAMLAKANT TRIPATHI  PINDARI ",
+      "Load": "10",
+      "Meter Slno": "Y0274439 \n"
+    }, False)
--- python/firebase.py
+++ python/firebase.py
@@ -1,49 +1,88 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import json
 
-#configurations begins
+# configurations begins
 projectId = "fir-project-sguha1988"
-serviceAccountJSONFilePath = "fir-project-sguha1988-firebase-adminsdk-3cha6-950836d42b.json"
-#configurations ends
+serviceAccountJSONFilePath = (
+    "fir-project-sguha1988-firebase-adminsdk-3cha6-950836d42b.json"
+)
+# configurations ends
 
-# cred = credentials.Certificate(serviceAccountJSONFilePath)
-# firebase_admin.initialize_app(cred)
 
-# db = firestore.client()
-# doc_ref = db.collection(u'users').document(u'alovelace')
-# doc_ref.set({
-#     'first': 'Ada',
-#     'last': 'Lovelace',
-#     'born': 1815
-# })
 class FirebaseDB:
     def __init__(self):
         self.serviceAccountJSONFilePath = serviceAccountJSONFilePath
         self.projectId = projectId
         self.__initializeDB()
+
+    # Private methods
     def __initializeDB(self):
         cred = credentials.Certificate(serviceAccountJSONFilePath)
         firebase_admin.initialize_app(cred)
+
     def __getDBInstance(self):
         dbInstance = firestore.client()
         return dbInstance
+
+    def __convertToCSV(self, data):
+        # todo
+        return data
+
+    ## Public method begins
     def write(self, collectionName, documentName, data, merge=True):
         dbInstance = self.__getDBInstance()
-        documentRefference = dbInstance.collection(collectionName).document(documentName)
+        documentRefference = dbInstance.collection(collectionName).document(
+            documentName
+        )
+        if merge == True:
+            print(len(self.read(collectionName, documentName)))
+        else:
+            dataToWrite = {}
+            items = []
+            items.append(data)
+            dataToWrite.update({"items":items})
+            documentRefference.set(dataToWrite)
+
+    def writeBulk(self, collectionName, documentName, data, merge=True):
+        dbInstance = self.__getDBInstance()
+        dataToWrite = {"items": data}
+        documentRefference = dbInstance.collection(collectionName).document(
+            documentName
+        )
         try:
-            if(merge == True):
-                documentRefference.set(data,  merge=True)
+            if merge == True:
+                documentRefference.set(dataToWrite, merge=True)
             else:
-                documentRefference.set(data)
+                documentRefference.set(dataToWrite)
             return True
         except:
-            print('Unable to write database')
-            
-    def read(self, collectionName, asJSON=True):
+            print("Unable to write database")
+
+    def readCSV(self, collectionName, docName=""):
+        dataToReturn = self.read(collectionName, docName)
+        # todo
+        return self.__convertToCSV(dataToReturn)
+
+    def readJSON(self, collectionName, docName=""):
+        dataToReturn = self.read(collectionName, docName)
+        return json.dumps(dataToReturn)
+
+    def read(self, collectionName, docName=""):
         dbInstance = self.__getDBInstance()
         collectionInstance = dbInstance.collection(collectionName)
         docs = collectionInstance.stream()
-        for doc in docs:
-            print(f'{doc.id} => {doc.to_dict()}')
-
+        docToReturn = []
+        if docName == "" or docName == "all":
+            for doc in docs:
+                for item in doc.to_dict()["items"]:
+                    docToReturn.append(item)
+                break
+        else:
+            for doc in docs:
+                if doc.id == docName:
+                    for item in doc.to_dict()["items"]:
+                        docToReturn.append(item)
+                    break
+        return docToReturn
